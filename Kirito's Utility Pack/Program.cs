8caa166bc86f103e3fae2d2e287bd1adcb47e880
Kirito Kun <seotox@gmail.com>
2015-07-13 19:07:39 +0200

    basic

---
+++ Kirito's Utility Pack/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kirito_s_Utility_Pack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
